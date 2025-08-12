213c13be0077b77771331e62311f3900b1fcaf7d
yeaseula <kminea@naver.com>
2025-08-13 01:46:39 +0900

    ♿️ 접근성 개선 : dialog 함수 변경

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
 window.addEventListener('DOMContentLoaded',()=>{
     const $ = (node) => document.querySelector(node);
 
+    //dialog modal function
     const emailInput = $('input[type="email"]');
     const modalOpenBtn = $('#modal-open-btn');
     const modalCloseBtn = $('#modal-close-btn');
+    const modalPop = $('.modal-submit');
 
+    modalOpenBtn.addEventListener('click',()=>{
+        if(emailInput.value == '') {
+            alert('이메일 주소를 입력하세요');
+        } else {
+            modalPop.showModal();
+        }
+    })
+
+    modalCloseBtn.addEventListener('click',()=>{
+        modalPop.close();
+    })
+
+    //side menu function - 모달 팝업과 같은 기능을 합니다
     function ModalFnc (modalPop) {
         const modalHas = modalPop.classList.contains('on');
         if(modalHas) {
@@ -13,21 +28,23 @@ window.addEventListener('DOMContentLoaded',()=>{
             modalPop.classList.add('on')
         }
     }
-
-    modalOpenBtn.addEventListener('click',()=>{
-        if(emailInput.value == '') {
-            alert('이메일 주소를 입력하세요');
-        } else {
-            const modalPop = $('.modal-submit');
-            ModalFnc (modalPop);
-        }
+    const HamburgerOpen = $('#hamburger-open-btn');
+    const HamburgerClose = $('#hamburger-close-btn')
+    HamburgerOpen.addEventListener('click',()=>{
+        const modalPop = $('#hamburger-menu');
+        ModalFnc(modalPop);
+    })
+    HamburgerClose.addEventListener('click',()=>{
+        const modalPop = $('#hamburger-menu');
+        ModalFnc(modalPop);
     })
 
-    modalCloseBtn.addEventListener('click',()=>{
-        const modalPop = $('.modal-submit');
-        ModalFnc (modalPop);
+    //scroll top
+    const ScrollTop = $('#scroll-top');
+    ScrollTop.addEventListener('click',()=>{
+        window.scrollTo({ top: 0, behavior: 'smooth' });
     })
-
+    //scroll event
     window.addEventListener('scroll', () => {
         const scrollTop = window.scrollY || document.documentElement.scrollTop;
         const scrollHeight = document.documentElement.scrollHeight;
@@ -48,20 +65,4 @@ window.addEventListener('DOMContentLoaded',()=>{
             scrollBtn.style.bottom = '24px';
         }
     });
-
-    const HamburgerOpen = $('#hamburger-open-btn');
-    const HamburgerClose = $('#hamburger-close-btn')
-    HamburgerOpen.addEventListener('click',()=>{
-        const modalPop = $('#hamburger-menu');
-        ModalFnc(modalPop);
-    })
-    HamburgerClose.addEventListener('click',()=>{
-        const modalPop = $('#hamburger-menu');
-        ModalFnc(modalPop);
-    })
-
-    const ScrollTop = $('#scroll-top');
-    ScrollTop.addEventListener('click',()=>{
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    })
 })
